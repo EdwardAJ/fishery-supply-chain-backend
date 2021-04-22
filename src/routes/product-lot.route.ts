658254eae6c688805d25e58d46bf6f4339dc1dba
EdwardAJ <13517115@std.stei.itb.ac.id>
2021-04-22 12:54:22 +0700

    refactor: change getProductLot naming to plural form

--- src/routes/product-lot.route.ts
+++ src/routes/product-lot.route.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import { authorizeToken } from "~/middleware/authorizeToken"
-import { getProductLots } from "~/controllers/product-lot/get/get-product-lot.controller"
+import { getProductLots } from "~/controllers/product-lot/get/get-product-lots.controller"
 
 const router = express.Router()
 router.get("/", authorizeToken, getProductLots)
