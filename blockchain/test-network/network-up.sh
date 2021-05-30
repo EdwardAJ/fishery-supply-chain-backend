16f65cc7ccc77a6a14a9d773b19257a58d949bdf
EdwardAJ <13517115@std.stei.itb.ac.id>
2021-05-30 18:32:10 +0700

    feat: change endorsement policy

--- blockchain/test-network/network-up.sh
+++ blockchain/test-network/network-up.sh
@@ -21,7 +21,7 @@ cd ..
 cd addOrg4
 ./addOrg4.sh up -c channel1 -ca -s couchdb
 cd ..
-./network.sh deployCC -c channel1 -ccn basic -ccp ../../chaincode -ccl typescript -ccep "AND('Org1MSP.member','Org2MSP.member','Org3MSP.member')"
+./network.sh deployCC -c channel1 -ccn basic -ccp ../../chaincode -ccl typescript -ccep "OutOf(2,'Org1MSP.member','Org2MSP.member','Org3MSP.member')"
 . scripts/addPeers.sh
 
 setAnchorPeer() {
